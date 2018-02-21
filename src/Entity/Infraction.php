f28e6a786308c32520c08a71f811fe93d6078e70
Nekzodem <remi260995@live.fr>
2018-02-21 15:36:51 +0100

    merge

--- src/Entity/Infraction.php
+++ src/Entity/Infraction.php
@@ -255,25 +255,6 @@ class Infraction
 
         return $this;
     }
-    /**
-     * Get the value of date_tribunal
-     */ 
-    public function getDateTribunal()
-    {
-        return $this->date_tribunal;
-    }
-
-    /**
-     * Set the value of date_tribunal
-     *
-     * @return  self
-     */ 
-    public function setDateTribunal($date_tribunal)
-    {
-        $this->date_tribunal = $date_tribunal;
-
-        return $this;
-    }
     public function __toString() {
         return $this->types_infractions;
     }
